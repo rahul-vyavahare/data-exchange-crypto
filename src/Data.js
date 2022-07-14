c30a24cf7543fb6fb77d0871142b92b8a1257d91
Rahul Vyavahare <rvyavahare.rv@gmail.com>
2022-07-14 22:12:23 +0530

    first commit

--- src/Data.js
+++ src/Data.js
@@ -1,8 +1,13 @@
+
 import React from "react";
+
 import './App.css';
+
+
 
 const ws = new WebSocket('wss://production-esocket.delta.exchange.');
 function App() {
+    
     const [data, setData] = React.useState([]);
     const [symbol, setSymbol] = React.useState([]);
     React.useEffect( () => {
@@ -10,12 +15,15 @@ function App() {
             .then((res) => res.json())
             .then((res) => {
                 setData(res.result);
-                // dd = [...res.result];
+                
                 let temp = [...res.result];
                 setSymbol(temp.map(item => item['symbol']));
             });
+        
     }, []);
-    React.useEffect(async () => {
+
+    
+    React.useEffect( () => {
 
 
         ws.onopen = (event) => {
@@ -41,40 +49,43 @@ function App() {
                 if ((jsn.mark_price) && [...data].length > 0) {
 
                     document.getElementById('' + jsn.symbol).innerHTML = "" + jsn.mark_price;
-                    //console.log(document.getElementById('' + jsn.symbol).innerHTML);
+                    
                     
                 }
 
             } catch (err) {
-                debugger
+                
                 console.log(err);
             }
         };
 
         //clean up function
-        //return () => ws.close();
+        return () => ws.close();
     }, [symbol]);
-  return (
-      <><table>
-          <thead>
-              <tr>
-                  <th>Symbol</th>
-                  <th>Description</th>
-                  <th>Underlying Asset</th>
-                  <th>Mark Price</th>
-              </tr>
-          </thead>
-          <tbody>
-              {data.map(item => <tr key={item.id}>
-                  <td>{item.symbol}</td>
-                  <td>{item.description}</td>
-                  <td>{item.underlying_asset.symbol}</td>
-                  <td id={'' + item.symbol} key={item.symbol + item.id}>loading...</td>
-              </tr>)}
-          </tbody>
-      </table>
-      </>
-  );
+
+
+    return (
+        <><table style={{width:'100%'}}>
+            <thead>
+                <tr style={{ width: '100%' }}>
+                    <th style={{ width: '20%' }}>Symbol</th>
+                    <th style={{ width: '40%' }}>Description</th>
+                    <th style={{ width: '20%' }}>Underlying Asset</th>
+                <th style={{ width: '20%' }}>Mark Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => <tr key={item.id}>
+                    <td>{item.symbol}</td>
+                    <td>{item.description}</td>
+                    <td>{item.underlying_asset.symbol}</td>
+                    <td id={'' + item.symbol} key={item.symbol+item.id}>loading...</td>
+                </tr>)}
+            </tbody>
+        </table>
+        </>
+
+    );
 }
 
 export default App;
